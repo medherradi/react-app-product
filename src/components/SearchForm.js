3d2d8c4eb7e7b743ffb611de6ade829400a4e43b
medherradi <med.herradi@gmail.com>
2023-01-13 14:08:13 -0100

    Search Form done

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,10 +3,25 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext()
+  const searchValue = React.useRef('')
+  React.useEffect(() =>
+    searchValue.current.focus()
+  )
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  const searchProduct = () => {
+    setSearchTerm(searchValue.current.value)
+  }
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your product</label>
+          <input type='text' id='name' ref={searchValue} onChange={searchProduct} />
+        </div>
+      </form>
+    </section>
   )
 }
 
