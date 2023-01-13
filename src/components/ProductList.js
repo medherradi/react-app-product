a99623774bfe263e27f7d36231670446a8085aae
medherradi <med.herradi@gmail.com>
2023-01-13 11:47:58 -0100

    handling useState and useEffect

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,6 +4,16 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const ProductList = () => {
+  const { products, loading } = useGlobalContext()
+
+  if (loading) {
+    return <Loading />
+  }
+  if (!products.length) {
+    return <h2 className='section-title'>
+      no phone matched your search ...
+    </h2>
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
--- src/context.js
+++ src/context.js
@@ -6,18 +6,25 @@ const url = 'https://dummyjson.com/products/search?q='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  const [products, setproduct] = useState([])
+  const [products, setproducts] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [loading, setloading] = useState(true)
 
   const fetchData = async () => {
+    setloading(true)
     try {
       const response = await axios.get(`${url}${searchTerm}`)
-      const data = response.data
+      const data = response.data.products
       console.log(data)
+      if (!data.length) {
+        setloading(false)
+      }
+      setproducts(data)
+      setloading(false)
 
     } catch (error) {
       console.log(error)
+      setloading(false)
     }
   }
 
@@ -25,7 +32,7 @@ const AppProvider = ({ children }) => {
     fetchData()
   }, [searchTerm])
 
-  return <AppContext.Provider value='hello'>
+  return <AppContext.Provider value={{ products, loading, setSearchTerm, }}>
     {children}
   </AppContext.Provider>
 }
